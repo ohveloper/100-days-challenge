84a7ef9d9036f3341aad8b1db3bd505d6ebafc08
ohveloper <ohveloper@gmail.com>
2022-09-04 23:25:18 +0900

    16일차 중간 커밋

---
+++ D16-OOP/main.py
@@ -0,0 +1,18 @@
+# import test_module
+# print(test_module.test)
+
+
+# import turtle
+from turtle import Turtle
+from turtle import Screen
+
+tommy = Turtle()
+print(tommy)
+tommy.shape("turtle")
+tommy.color("DarkOrchid2")
+tommy.forward(100)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
