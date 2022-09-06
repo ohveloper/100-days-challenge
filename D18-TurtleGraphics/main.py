711eaaabda7dfd268c075c09480d3866f41d119d
ohveloper <ohveloper@gmail.com>
2022-09-07 00:30:29 +0900

    D18 중간 커밋

---
+++ D18-TurtleGraphics/main.py
@@ -0,0 +1,108 @@
+# 이 방식도 한번만 호출 하는것 이라면 가급적 사용하지 말자.
+# 중간에 작성된 코드의 혼란을 막기 위해서
+# from turtle import Turtle, Screen
+
+# alias 활용
+import turtle as t
+import random as r
+# import heroes
+
+# 모듈에 담겨있는 모것을 임포트 할수 있음
+# 단점은 어디서 import된 것인지 알수 없다는것!
+# 사용하지 말자
+# from turtle import *
+
+tim = t.Turtle()
+tim.shape("turtle")
+# tim.hideturtle()
+tim.color("purple1")
+
+# challenge 1 : make square
+# for _ in range(4):
+#     tim.fd(100)
+#     tim.right(90)
+
+# challenge 2 : make 점선
+# flag = -1
+# for _ in range(15):
+#     if flag == -1:
+#         tim.pencolor("black")
+#     else:
+#         tim.pencolor("white")
+#     tim.fd(10)
+#     flag *= -1
+
+# 문서를 꼼꼼히 읽어보자.
+# 요구 조건을 꼼꼼히 살피자
+# for _ in range(15):
+#     tim.fd(10)
+#     tim.penup()
+#     tim.fd(10)
+#     tim.pendown()
+
+# challenge 3 : 삼각형 ~ 10각형까지 도형을 순서대로 각각 다른 색상으로 그린다.
+# circle = 360
+# for i in range(3,11):
+#     angle = circle / i
+#     pen_color = (r.random(), r.random(), r.random())
+#     for _ in range(i):
+#         tim.pencolor(pen_color)
+#         tim.right(angle)
+#         tim.fd(100)
+
+# challenge 4 : Draw a Random Walk
+# 선 더 굵게, 더 빠르게
+
+# turn = [0, 90, 180, 270] # 360을 0으로 바꾸자
+# tim.speed(0)
+# tim.pensize(10)
+# for _ in range(300):
+#     pen_color = (r.random(), r.random(), r.random())
+#     print(pen_color)
+#     tim.pencolor(pen_color)
+#     tim.fd(20)
+#     tim.setheading(r.choice(turn))
+
+
+# challenge 5 : 튜플 활용해서 r,g,b 컬러 생성하는 함수 만들자.
+# tim.speed(0)
+# tim.pensize(10)
+# t.colormode(255)
+# def random_color():
+#     red = r.randint(0, 255)
+#     green = r.randint(0, 255)
+#     blue = r.randint(0, 255)
+#     return (red, green, blue)
+#
+# turn = [0, 90, 180, 270]
+# for _ in range(200):
+#     pen_color = random_color()
+#     tim.pencolor(pen_color)
+#     tim.fd(20)
+#     tim.setheading(r.choice(turn))
+
+
+# challenge 6 : Make a Spirograph
+
+t.colormode(255)
+def random_color():
+    red = r.randint(0, 255)
+    green = r.randint(0, 255)
+    blue = r.randint(0, 255)
+    return (red, green, blue)
+
+t.speed(0)
+t.pensize(1)
+
+for i in range(0, 360, 5):
+    t.pencolor(random_color())
+    t.circle(120, 360)
+    t.setheading(i)
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
---
+++ D18-TurtleGraphics/note.md
@@ -0,0 +1,12 @@
+- from turtle import *
+
+- 모듈에 담겨있는 모것을 임포트 할수 있음
+- 단점은 어디서 import된 것인지 알수 없다는것!
+- 사용하지 말자!
+
+## Tuple
+- 특징
+  - index값을 갖는다
+  - immutable 하다
+  바위에 새긴거같이
+  - list(my_tuple) -> list로 변환 할 수 있다.!!!
