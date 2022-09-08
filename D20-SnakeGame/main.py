1515cad273c80db27c1ce606438c9b6c66d6ff79
ohveloper <ohveloper@gmail.com>
2022-09-09 01:05:25 +0900

    스네이크 클래스로 이동 완료

--- D20-SnakeGame/main.py
+++ D20-SnakeGame/main.py
@@ -1,5 +1,6 @@
+from turtle import Screen
+from snake import Snake
 import time
-from turtle import Screen, Turtle
 
 
 screen = Screen()
@@ -8,36 +9,17 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_postition =[(0,0), (-20, 0), (-40, 0)]
-
-segments = []
-
-# 세그먼트들을 생성해서 최초 위치에 두고 위한 반복문
-for position in starting_postition:
-    new_segment = Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.setposition(position)
-    segments.append(new_segment)
+snake = Snake()
 
 is_game_on = True
-
 while is_game_on:
     # .tracer()로 멈췄던 애니메이션을 다시 작동 시기큰 함수
     screen.update()
     # 잠시 멈췄다가 다시 진행 시키는 함수
     time.sleep(0.1)
 
-    # 맨뒤 뱀부터 바로 앞전 뱀위치로 옮기기 위한 반복문
-    # range() 함수 뒤에서부터 카운트 하는법
-    for segment_num in range(len(segments)-1, 0, -1):
-        # 현재 세그먼트 바로 앞전 세그먼트의 x,y 값을 추출해서 현재 세그먼트의 위치를 수정
-        new_x = segments[segment_num - 1].xcor()
-        new_y = segments[segment_num - 1].ycor()
-        segments[segment_num].goto(new_x, new_y)
-    # 맨 앞 세그먼트를 앞으로 이동
-    segments[0].fd(20)
-    # segments[0].right(90)
+    snake.move()
+
 
 
 
--- D20-SnakeGame/snake.py
+++ D20-SnakeGame/snake.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
+STARTING_POSITION = [(0,0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 class Snake:
     def __init__(self):
-        self.starting_postion = [(0,0), (-20, 0), (-40, 0)]
         self.segments = []
+        self.make_snake()
 
     def make_snake(self):
-        for position in self.starting_postition:
+        for position in STARTING_POSITION:
             new_segment = Turtle("square")
             new_segment.penup()
             new_segment.color("white")
@@ -14,11 +16,11 @@ class Snake:
             self.segments.append(new_segment)
 
     def move(self):
-        self.make_snake()
         for segment_num in range(len(self.segments) - 1, 0, -1):
             # 현재 세그먼트 바로 앞전 세그먼트의 x,y 값을 추출해서 현재 세그먼트의 위치를 수정
             new_x = self.segments[segment_num - 1].xcor()
             new_y = self.segments[segment_num - 1].ycor()
             self.segments[segment_num].goto(new_x, new_y)
         # 맨 앞 세그먼트를 앞으로 이동
-        self.segments[0].fd(20)
+        self.segments[0].fd(MOVE_DISTANCE)
+        # self.segments[0].right(90)
