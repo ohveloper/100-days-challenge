0a1bc59d2855f9e5907392d5ec2d1fae927f13cf
ohveloper <ohveloper@gmail.com>
2022-09-12 23:29:10 +0900

    D24 daily

--- D24-FileDirectoryPath/Mail Merge Project Start/main.py
+++ D24-FileDirectoryPath/Mail Merge Project Start/main.py
@@ -1,17 +1,17 @@
 
-with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt") as starting_letter:
+with open("./Input/Letters/starting_letter.txt") as starting_letter:
     # contents = starting_letter.read()
     contents = starting_letter.read()
     print(contents)
 
-    with open("../Mail Merge Project Start/Input/Names/invited_names.txt") as invited_name:
+    with open("./Input/Names/invited_names.txt") as invited_name:
         name_contents = invited_name.read()
         name_list = name_contents.split("\n")
         # print(name_contents)
         # print(name_list)
 
         for name in name_list:
-            with open(f"../Mail Merge Project Start/Output/ReadyToSend/{name}.txt", mode="w") as mail:
+            with open(f"./Output/ReadyToSend/{name}.txt", mode="w") as mail:
                 x = contents.replace("[name]", name)
                 mail.write(x)
                 print(x)
