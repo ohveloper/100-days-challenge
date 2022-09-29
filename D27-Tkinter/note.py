0fff7a6e1d5cb0b0250f4fe3f06ec80d4b81e220
ohveloper <ohveloper@gmail.com>
2022-09-29 22:49:24 +0900

    D27

--- D27-Tkinter/note.py
+++ D27-Tkinter/note.py
@@ -1,6 +1,6 @@
 # Unlimited Arguments
 
-def add(*args):
+def add(*args):  # args type is tuple
     result = 0
     for n in args:
         result += n
@@ -8,3 +8,29 @@ def add(*args):
 
 result = add(1,2,3,4,5)
 print(result)
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(3, add=3, multiply=10)
+
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+
+        # get으로 받아오면 인자를 받지 않았을때 오류가 나지 않는다.
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+my_car = Car(make="Kia")
+print(my_car.make)
